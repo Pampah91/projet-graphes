e6238598bb707afd64cd1b7dd57da0add5edc7ab
LE FLOCH Kévin <kevin.le.floch@efrei.net>
2020-04-15 00:13:47 +0200

    Récup données sans transitions + Menu,rebouclages et mise en forme

--- src/graphes/Graph.java
+++ src/graphes/Graph.java
@@ -20,6 +20,8 @@ public int[] sommets_terminaux;
 public int[] nbre_transitions;
 public String[] transitions;
 
+//static int arret_readAndStore;// stop la fonction à 1, continue le programme à 0
+
 	/*public int compteur_taille() {
 		
 		return 1;
@@ -55,18 +57,17 @@ public String[] transitions;
 	
 	public void readAndStore() {
 		
-		memo_length_SI_SF = new int[2];// tableau qui stockera le nbre d'information de EI en [0] et EF en [1]
-		
+	memo_length_SI_SF = new int[2];// tableau qui stockera le nbre d'information de EI en [0] et EF en [1]
+	int restart;// recommence la demande de graphe si restart=1 (lorque l'utilisateur se trompe sur le nom du fichier texte), si restart = 0 alors passons à la suite
+	String filename = null;
+	
+	do {
 		try {
-			  String filename = null;
-			  System.out.println("Quel graphe voulez vous ouvrir ? Exemple : graphe1.txt \nVotre graphe : ");
-			  
+              System.out.println("Quel graphe voulez vous ouvrir ? Exemple : graphe1.txt \nVotre graphe : ");
 			  Scanner input = new Scanner(System.in);
-			  filename = input.nextLine();
-			  input.close();
-			  
+              filename = input.nextLine();
 			  System.out.println("");
-			  
+		
 		      File file = new File(filename);
 		      Scanner myReader = new Scanner(file);
 		      
@@ -91,7 +92,7 @@ public String[] transitions;
 		            	System.out.println(valeurs_diff_arcs[i]); 
 		               	i++;
 		            } 
-		            	//System.out.println(line); // on affiche la ligne
+		            	
 	        	}
 	        	
 	        	else if (data.contains("Nbre_sommets")) 
@@ -103,11 +104,9 @@ public String[] transitions;
 		            
 		            while(val.hasMoreTokens()) //tant que il y a encore des valeurs
 		            { 
-		            	
 		            	nbre_sommets[i] = Integer.parseInt(val.nextToken()); //le tableau prend l'entier
 		            	System.out.println(nbre_sommets[i]);// on affiche avec le get la valeur de Sortie
 		            	i++;
-
 		            } 
 		            //System.out.println(line);	  
 	        	}
@@ -119,9 +118,6 @@ public String[] transitions;
 	        		
 	        		sommets_initiaux = new int[10];
 		            data = myReader.nextLine();//sauter la ligne
-		            
-		            
-		            
 		            StringTokenizer val = new StringTokenizer(data," "); // ici point important: ," " indique qu'on utilise le séparateur de mots (de valeurs) espace.
 		            
 		            while(val.hasMoreTokens()) //tant que il y a encore des valeurs
@@ -139,19 +135,58 @@ public String[] transitions;
 		           //System.out.println(line);	 
 	        	}
 	        	
+	        	else if (data.contains("Sommets_terminaux")) 
+	        	{
+	        		int i = 0; 
+	        		int compt_taille_sespace = 0;// compteur pour connaître le nombre d'informations/valeurs de EI
+	        		
+	        		sommets_terminaux = new int[10];
+		            data = myReader.nextLine();//sauter la ligne
+		            StringTokenizer val = new StringTokenizer(data," "); // ici point important: ," " indique qu'on utilise le séparateur de mots (de valeurs) espace.
+		            
+		            while(val.hasMoreTokens()) //tant que il y a encore des valeurs
+		            { 
+		            	sommets_terminaux[i] = Integer.parseInt(val.nextToken()); //le tableau prend l'entier
+		            	System.out.print(sommets_terminaux[i]+" ");// on affiche avec le get la valeur de Sortie
+		            	i++;
+		            	compt_taille_sespace++;
+		            } 
+		            
+		            System.out.println(" ");// afficher un saut de ligne
+		            
+		            memo_length_SI_SF[1]=compt_taille_sespace; 
+	        	}
+	        	
+	        	else if (data.contains("Nbre_transitions")) 
+	        	{
+	        		int i = 0;
+	        		nbre_transitions = new int[1];
+		            data = myReader.nextLine();//sauter la ligne	            		            	
+		            StringTokenizer val = new StringTokenizer(data," "); // ici point important: ," " indique qu'on utilise le séparateur de mots (de valeurs) espace.
+		            
+		            while(val.hasMoreTokens()) //tant que il y a encore des valeurs
+		            { 
+		            	nbre_transitions[i] = Integer.parseInt(val.nextToken()); //le tableau prend l'entier
+		            	System.out.println(nbre_transitions[i]);// on affiche avec le get la valeur de Sortie
+		            	i++;
+		            } 
+		            //System.out.println(line);	  
+	        	}
+	        	
 		      }
 		      myReader.close();
 		      
 		      System.out.println("----FIN du fichier texte du graphe---- ");
+		      restart = 0;
 		      
-		    } catch (FileNotFoundException e) 
+		    }catch (FileNotFoundException e) 
 		      {
-		      System.out.println("Une erreur est survenue ! Impossible de lire le fichier.");
-		      e.printStackTrace();
-		      }
-		  
-	}
-	
-	
+		       System.out.println("Erreur! Le fichier \" "+filename+" \" n'existe pas.");
+		       restart=1;
+		       //e.printStackTrace();
+		  }
 		
+	   }while(restart != 0);
 	}
+			
+}
--- src/graphes/Main.java
+++ src/graphes/Main.java
@@ -1,5 +1,9 @@
 package graphes;
 
+//import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.NoSuchElementException;
+
 public class Main {
 
 	/*public static int[] valeurs_diff_arcs;
@@ -12,27 +16,69 @@ public class Main {
 	
 	public static void main(String[] args){
 		
-		int i = 0;
 		
-		g.readAndStore();
-		System.out.println("");
-		
-		//test sauvegarde info lecture
-
-		System.out.println("Il y a "+g.valeurs_diff_arcs[0]+" valeur(s) différente(s) sur les arcs");
-
-		System.out.println("Il y a "+g.nbre_sommets[0]+" sommet(s)");
-
-		System.out.println("Test memo_length :"+ g.memo_length_SI_SF[0]);//test
-		
-		System.out.println("On a :"+ g.sommets_initiaux[i]+" sommets qui n'ont pas de prédécesseur");
-		
-		System.out.print("Le(s) sommet(s) qui n'a(n'ont) pas de successeur sont : ");
-		
-		for(i=1;i<g.memo_length_SI_SF[0];i++)// On s'arrete aux nombres totales d'informations de la ligne.
+	try {
+			int i = 0;
+			int arret_programme = 1;// stop la fonction à "0", continue le programme à "1"
+				
+		while(arret_programme != 0 && arret_programme == 1)
 		{
-			System.out.print(g.sommets_initiaux[i]+", ");
+			g.readAndStore();
+			System.out.println("");
+			
+		//test sauvegarde info lecture
+			System.out.println("Il y a "+g.valeurs_diff_arcs[0]+" valeur(s) différente(s) sur les arcs");
+
+			System.out.println("Il y a "+g.nbre_sommets[0]+" sommet(s)");
+
+			//affichage Sommet initiaux
+			//System.out.println("Test memo_length[0] :"+ g.memo_length_SI_SF[0]);//test
+			
+			System.out.println("On a :"+ g.sommets_initiaux[0]+" sommets qui n'ont pas de prédécesseur");
+			
+			System.out.print("Le(s) sommet(s) qui n'a(n'ont) pas de prédécesseur est (sont) : ");
+			
+			for(i=1;i<g.memo_length_SI_SF[0];i++)// On s'arrete aux nombres totales d'informations de la ligne.
+			{
+				System.out.print(g.sommets_initiaux[i]+", ");
+			}
+			
+			System.out.println(" ");// saut de ligne
+			
+			//affichage Sommets terminaux
+			//System.out.println("Test memo_length[1] :"+ g.memo_length_SI_SF[1]);//test
+			
+			System.out.println("On a :"+ g.sommets_terminaux[0]+" sommets qui n'ont pas de successeur");
+			
+			System.out.print("Le(s) sommet(s) qui n'a(n'ont) pas de successeur est (sont) : ");
+			
+			for(i=1;i<g.memo_length_SI_SF[1];i++)// On s'arrete aux nombres totales d'informations de la ligne.
+			{
+				System.out.print(g.sommets_terminaux[i]+", ");
+			}
+			
+			System.out.println(" ");
+			System.out.println("Il y a "+g.nbre_transitions[0]+" transition(s)");
+			
+		do {
+			 System.out.println(" Voullez-vous changer de graphe? Si oui, tapez \"1\". Sinon, arreter le programme en tapant \"0\" ");
+			 Scanner input_arretP = new Scanner(System.in);
+			 arret_programme = input_arretP.nextInt();
+			 
+			 if(arret_programme == 0 )
+			 {
+			   System.out.println("Fin du programme");
+			 }
+			
+		   }while(arret_programme != 0 && arret_programme != 1);
+			
+			  System.out.println("");
 		}
+		}catch (NoSuchElementException e) 
+	      {
+	       System.out.println("Caca");
+	       e.printStackTrace();
+	      }
 		
 	}
 
